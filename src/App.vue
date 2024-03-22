3ce096434eca6366a29af58e5a73e1c9c4829b4c
Nikola Jankovic <nikolakrakajankovic@gmail.com>
2024-03-22 03:44:52 +0100

    Formatting done better/

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,28 @@
 <template>
   <h1>My Top Five</h1>
   <nav>
-   <ul>
-    <li>Home</li>
-    <li>Contact</li>
-    <li>About Us</li>
-   </ul>
+    <ul>
+      <li>Home</li>
+      <li>Contact</li>
+      <li>About Us</li>
+    </ul>
   </nav>
   <main>
-  <p>The Beatles</p>
-  <img src="" alt="">
-  <div>
+    <p>The Beatles</p>
+    <img src="" alt="" />
+    <div></div>
+    <p>Pink Floyd</p>
+    <img src="" alt="" />
+    <div></div>
+    <p>The Prince</p>
+    <img src="" alt="" />
+    <div></div>
 
-
-  </div>
-  <p>Pink Floyd</p>
-  <img src="" alt="">
-  <div>
-
-  </div>
-  <p>The Prince</p>
-  <img src="" alt="">
-  <div>
-
-  </div>
-
-  <p>Radiohead</p>
-  <img src="" alt="">
-  <div>
-
-  </div>
-  <p>Sting</p>
-  <img src="" alt="">
-  <div>
-
-  </div>
-
+    <p>Radiohead</p>
+    <img src="" alt="" />
+    <div></div>
+    <p>Sting</p>
+    <img src="" alt="" />
+    <div></div>
   </main>
 </template>
-
-
