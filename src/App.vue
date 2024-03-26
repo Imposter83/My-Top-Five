c2bcde1ceb0ea70d4d1d74d8bc41b68b558c2170
Nikola Jankovic <nikolakrakajankovic@gmail.com>
2024-03-26 05:04:16 +0100

    artists images/front page

--- src/App.vue
+++ src/App.vue
@@ -4,25 +4,62 @@
     <ul>
       <li>Home</li>
       <li>Contact</li>
-      <li>About Us</li>
+      <li>About Me</li>
     </ul>
   </nav>
   <main>
     <p>The Beatles</p>
-    <img src="" alt="" />
-    <div></div>
+    <div class="container">
+      <img src="@/assets/images/Beatles.png" />
+    </div>
     <p>Pink Floyd</p>
-    <img src="" alt="" />
-    <div></div>
+    <div class="container">
+      <img src="@/assets/images/pink-floyd-seeklogo.png" />
+    </div>
     <p>The Prince</p>
-    <img src="" alt="" />
-    <div></div>
-
+    <div class="container">
+      <img src="@/assets/images/Prince.png" />
+    </div>
     <p>Radiohead</p>
-    <img src="" alt="" />
-    <div></div>
+    <div class="container">
+      <img src="@/assets/images/Radiohead.png" />
+    </div>
     <p>Sting</p>
-    <img src="" alt="" />
-    <div></div>
+    <div class="container">
+      <img src="@/assets/images/Sting.jpg" />
+    </div>
   </main>
 </template>
+<style>
+.container {
+  width: 1000px;
+  height: auto;
+}
+
+.container img {
+  max-width: 100%;
+  max-height: 100%;
+  display: blockS;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+li {
+  display: inline;
+  margin-right: 10px;
+}
+h1 {
+  color: blanchedalmond;
+  background-color: blue;
+  text-align: center;
+  font-family: verdana;
+  font-size: 300%;
+}
+p {
+  color: blanchedalmond;
+  background-color: blue;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 160%;
+}
+</style>
