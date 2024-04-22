744fa36fac6d20c71b92f84d272cf4ca32424132
Nikola Jankovic <nikolakrakajankovic@gmail.com>
2024-04-22 15:05:15 +0200

    homeview routing done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,25 +6,29 @@
   <main>
     <!-- <TheWelcome /> -->
     <h1>MyTopFive</h1>
-    <h2>The Beatles</h2>
+    <h2 @click="$router.push('/beatles')">The Beatles</h2>
     <div class="container"></div>
     <img src="@/assets/images/Beatles.png" />
-    <h2>Pink Floyd</h2>
+    <h2 @click="$router.push('/pink-floyd')">Pink Floyd</h2>
     <div class="container">
       <img src="@/assets/images/Pink Floyd.jpg" />
     </div>
-    <h2>The Prince</h2>
+    <h2 @click="$router.push('/prince')">The Prince</h2>
     <div class="container">
       <img src="@/assets/images/Prince.png" />
     </div>
-    <h2>Radiohead</h2>
+    <h2 @click="$router.push('/radiohead')">Radiohead</h2>
     <div class="container">
       <img src="@/assets/images/Radiohead.jpg" />
     </div>
-    <h2>Sting</h2>
+    <h2 @click="$router.push('/sting')">Sting</h2>
     <div class="container">
       <img src="@/assets/images/Sting.jpg" />
     </div>
   </main>
 </template>
-<style scoped></style>
+<style scoped>
+  h2:hover {
+    background-color: lightgray;
+  }
+</style>
